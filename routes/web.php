5ac1240e25eed8a0ba7990524037e21474938d8b
rick672 <aliagaricardo321@gmail.com>
2025-11-20 12:02:19 -0400

    Instalación y Personalización del Menú

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin', function(){
+    return view('admin.index');
+});
