03fcb52530025fb9e9b83c2b51e52e49636d31f4
rick672 <aliagaricardo321@gmail.com>
2025-12-01 16:08:21 -0400

    Mejora al Dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $total_roles = Role::count();
+        $total_usuarios = User::whereDoesntHave('roles', function ($query){
+            $query->where('name', 'Super Admin');
+        } )->count();
+        return view('admin.index', compact('total_roles', 'total_usuarios'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,77 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>Bienvenido {{ Auth::user()->name }}</h1>
+    <div class="row">
+        <div class="col-12 col-md-6 order-md-1 order-last">
+            <h1>Bienvenid@, {{ Auth::user()->name }}</h1>
+            <p class="text-subtitle text-muted"></p>
+        </div>
+        <div class="col-12 col-md-6 order-md-2 order-first">
+            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="">Rol</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">{{ Auth::user()->roles->pluck('name')->implode(', ') }}</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 col-lg-9">
+            <div class="row">
+                <div class="col-12 col-sm-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-4 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                    <a href="{{ route('admin.roles.index') }}">
+                                        <div class="stats-icon blue mb-2">
+                                            <i class=""><i class="bi bi-shield-lock-fill"></i></i>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                    <h6 class="text-muted font-semibold">Roles registrados</h6>
+                                    <h6 class="font-extrabold mb-0">{{ $total_roles }}</h6>
+                                </div>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-4 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                    <a href="{{ route('admin.usuarios.index') }}">
+                                        <div class="stats-icon green mb-2">
+                                            <i class=""><i class="bi bi-person-fill-add"></i></i>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                    <h6 class="text-muted font-semibold">Usuarios registrados</h6>
+                                    <h6 class="font-extrabold mb-0">{{ $total_usuarios }}</h6>
+                                </div>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-3">
+            <div class="card">
+                <div class="card-body py-4 px-4">
+                    <div class="d-flex align-items-center">
+                        <div class="avatar avatar-xl">
+                            <img src="./assets/compiled/jpg/1.jpg" alt="Face 1">
+                        </div>
+                        <div class="ms-3 name">
+                            <h5 class="font-bold">John Duck</h5>
+                            <h6 class="text-muted mb-0">@johnducky</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
